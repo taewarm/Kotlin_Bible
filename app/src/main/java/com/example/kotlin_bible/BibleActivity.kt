79c1826708f361027933bd804572ca6aa229e7fd
Kimtaewon <taewarm@github.com>
2021-04-15 16:32:31 +0900

    bible api update

--- app/src/main/java/com/example/kotlin_bible/BibleActivity.kt
+++ app/src/main/java/com/example/kotlin_bible/BibleActivity.kt
@@ -19,23 +19,15 @@ import kotlinx.android.synthetic.main.dialog_bible.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.coroutines.coroutineContext
 
-class BibleActivity : AppCompatActivity(){
+open class BibleActivity : AppCompatActivity(){
     var page = arrayListOf<String>()
     var row = arrayListOf<String>()
     var content = arrayListOf<String>()
 
     var contentt :String =""
 
-    var page_s = arrayListOf<String>("1","2","3","4","5","6")
-    var row_s = arrayListOf<String>("1","2","3","4","5","6")
-    var page_e = arrayListOf<String>("1","2","3","4","5","6")
-    var row_e = arrayListOf<String>("1","2","3","4","5","6")
-    var contents = arrayListOf<String>()
-    var page1: Int = 1
-    var row1: Int = 1
-    var page2: Int = 1
-    var row2: Int = 1
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,26 +36,24 @@ class BibleActivity : AppCompatActivity(){
         val bititle = intent.getStringExtra("Bible").toString()
         val vallue = intent.getStringExtra("Value").toString()
         val bicontent = intent.getStringExtra("Content").toString()
-        //spinnerinit()
-
+        val dialog = CustomDialog(this)
         TWAPIstart(vallue)
         if(bicontent == "null"){//null을 문자열로 한이유가 Content는 값이 null인데 toString으로 문자열로 바꿈
-            bible_content.setText("시작")
+            bible_content1.setText("시작")
         }else{
-            bible_content.setText(bicontent)
+            bible_content1.setText(bicontent)
         }
 
         bible_title.setText(bititle)
         btn_sel.setOnClickListener {
             val nextIntent = Intent(this, ListActivity::class.java)
             startActivity(nextIntent)
-//            val dialog = CustomDialog(this)
-//            dialog.myDig()
+
         }
         btn_search.setOnClickListener {
-            //scl_bible.smoothScrollTo(0,5000)
+//            scl_bible.smoothScrollTo(0,bible_content2.top);
             Log.i("여기",scl_bible.scrollY.toString())
-
+            dialog.myDig()
         }
         //스크롤 움직일때마다 y좌표값 로그 출력
 //        scl_bible.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
@@ -90,7 +80,11 @@ fun TWAPIstart(key :String){
                     Log.i("여기",page[i]+","+row[i]+","+content[i])
                     contentt += row[i]+"절 :"+content[i]
                 }
-                bible_content.setText(contentt)
+                bible_content1.setText(page[0]+"ㅈ"+row[0]+"ㅈ"+content[0])
+                bible_content2.setText(page[1]+"ㅈ"+row[1]+"ㅈ"+content[1])
+                bible_content3.setText(page[2]+"ㅈ"+row[2]+"ㅈ"+content[2])
+                bible_content4.setText(page[3]+"ㅈ"+row[3]+"ㅈ"+content[3])
+                bible_content5.setText(page[4]+"ㅈ"+row[4]+"ㅈ"+content[4])
             }
         }
 
@@ -101,88 +95,35 @@ fun TWAPIstart(key :String){
     })
 
 }
-fun spinnerinit(){
-    spn_page_s.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,page_s)
-    spn_page_s.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-        override fun onItemSelected(
-            parent: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            Log.i("여기",page_s[position])
-            page1 = page_s[position].toInt()
-        }
+}
+class CustomDialog(context: Context){
+    private val dialog = Dialog(context)
+    var tae1 : String =""
+    var tae2 : String =""
+    val list = arrayListOf<String>("1","2","3")
+    val list1 = arrayListOf<String>("1","2","3")
+    val myAdapter = ArrayAdapter(dialog.context,android.R.layout.simple_spinner_dropdown_item,list)
+    val myAdapter1 = ArrayAdapter(dialog.context,android.R.layout.simple_spinner_dropdown_item,list1)
+    fun myDig(){
+        dialog.setContentView(R.layout.dialog_bible)
 
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            Log.i("여기","앙골라")
+        val spp_list = dialog.sp_list
+        val spp_list1 = dialog.sp_list1
+        spp_list.adapter = myAdapter
+        spp_list1.adapter = myAdapter1
+        val asd = BibleActivity()
+        dialog.window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT)
+        val btn_Ok = dialog.btn_ok
+        btn_Ok.setOnClickListener {
+            Log.i("여기","실행")
+            val sel = spp_list.selectedItemPosition
+            val sel1 = spp_list1.selectedItemPosition
+            tae1 = spp_list.getItemAtPosition(sel).toString()
+            tae2 = spp_list1.getItemAtPosition(sel1).toString()
+            dialog.hide()
         }
-    }
-    spn_row_s.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,row_s)
-    spn_row_s.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-        override fun onItemSelected(
-            parent: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            Log.i("여기",row_s[position])
-            row1 = row_s[position].toInt()
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            Log.i("여기","앙골라")
-        }
-    }
-    spn_page_e.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,page_e)
-    spn_page_e.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-        override fun onItemSelected(
-            parent: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            Log.i("여기",page_e[position])
-            page2 = page_e[position].toInt()
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            Log.i("여기","앙골라")
-        }
-    }
-
-    spn_row_e.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,row_e)
-    spn_row_e.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-        override fun onItemSelected(
-            parent: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            Log.i("여기",row_e[position])
-            row2 = row_e[position].toInt()
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            Log.i("여기","앙골라")
-        }
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setCancelable(true)
+        dialog.show()
     }
 }
-}
-//class CustomDialog(context: Context): BibleActivity() {
-//    var list = arrayListOf<Bible>(
-//        Bible("마복"),Bible("출굽")
-//    )
-//
-//
-//    val dialog = Dialog(context)
-//    fun myDig(){
-//
-//        dialog.setContentView(R.layout.dialog_bible)
-//        lsv_bible.adapter = BibleAdapter(dialog.context,list)
-//        dialog.window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT)
-//        dialog.setCanceledOnTouchOutside(true)
-//        dialog.setCancelable(true)
-//        dialog.show()
-//    }
-//}
